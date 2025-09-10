87c05d08bfd02d515807a9917d10b3e140935ef8
AlexDukshanin <alexdukshanin@yandex.ru>
2025-09-10 22:31:00 +0700

    start work 4.1 fix

--- src/lesson4/lesson4.1.kt
+++ src/lesson4/lesson4.1.kt
@@ -1,8 +1,16 @@
-package lesson4
+fun main() {
+    val numbers = intArrayOf(34, 12, 89, 5, 67, 23, 91, 5, 18)
 
-fun main (){
+    // Находим максимальное значение и индекс
+    val maxValue = numbers.maxOrNull() ?: 0
+    val maxIndex = numbers.indexOf(maxValue)
 
+    val minValue = numbers.minOrNull() ?: 0
+    val minIndex = numbers.indexOf(minValue)
 
+    println("Максимальное значение: $maxValue")
+    println("Индекс максимального значения: $maxIndex")
 
-
+    println("Минимальное значение: $minValue")
+    println("Индекс мимимального значения: $minIndex")
 }
