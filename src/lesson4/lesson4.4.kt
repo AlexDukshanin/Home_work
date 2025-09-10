8d6d7f6de2d66f6dc1bdfff2956485ab1d2b8b30
AlexDukshanin <alexdukshanin@yandex.ru>
2025-09-10 23:03:01 +0700

    start work 4.4 fix

--- src/lesson4/lesson4.4.kt
+++ src/lesson4/lesson4.4.kt
@@ -2,7 +2,7 @@ package lesson4
 
 fun main () {
     val num = intArrayOf(0, 1, 0, 1, 0)
-    if (num.toList() == num.reversed().toList()) {
+    if (num.toString() == num.reversed().toString()) {
         println ("палиндром")
     } else
         println ("не палиндром")
