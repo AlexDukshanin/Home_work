a74741c840cd65e1e3ff537ffdd05330283a278b
AlexDukshanin <alexdukshanin@yandex.ru>
2025-09-11 14:08:24 +0700

    5.2 finished fix

--- src/lesson5/lesson5.2.kt
+++ src/lesson5/lesson5.2.kt
@@ -1,65 +1,26 @@
 package lesson5
 
 fun main() {
-    println("КОНВЕРТЕР ТЕКСТА В ASCII")
+    // 1. Текст в коды
+    println("Введите слово:")
+    val word = readln()
 
-    print("Введите текст: ")
-    val inputText = readln()
-
-    println()
-    println("Результат преобразования:")
-
-    val codes = mutableListOf<Int>()
-    for (char in inputText) {
-        val code = char.code
-        codes.add(code)
-        print("$code ")
+    println("Коды букв:")
+    for (letter in word) {
+        println("$letter = ${letter.code}")
     }
 
-    println("\n")
-    println("Итоговая последовательность: ${codes.joinToString(" ")}")
+    println("\n" + "*".repeat(20))
 
-    println("\n" + "=".repeat(50))
-    convertAsciiToText()
-}
+    // 2. Коды в текст
+    println("Введите коды через пробел:")
+    val codes = readln().split(" ")
 
-fun convertAsciiToText() {
-    println("ПРЕОБРАЗОВАНИЕ ASCII-КОДОВ В ТЕКСТ")
-
-
-    print("Введите ASCII-коды через пробел: ")
-    val inputCodes = readln()
-
-
-    val codeStrings = inputCodes.split(" ")
-    println("Нашли коды: $codeStrings")
-
-    // Создаем переменную для результата
-    var resultText = ""
-
-    // Преобразуем каждый код в символ
-    println("\nПреобразуем коды в символы:")
-
-    for (codeStr in codeStrings) {
-        if (codeStr.isNotEmpty()) { // Пропускаем пустые строки
-            try {
-                // Преобразуем строку в число
-                val code = codeStr.toInt()
-
-                // Преобразуем число в символ
-                val char = code.toChar()
-
-                // Добавляем символ к результату
-                resultText += char
-
-                // Показываем процесс
-                println("Код $code → Символ '$char'")
-            } catch (e: NumberFormatException) {
-                println("Ошибка: '$codeStr' не является числом!")
-            }
+    var result = ""
+    for (code in codes) {
+        if (code.isNotEmpty()) {
+            result += code.toInt().toChar()
         }
     }
-
-    println("\nРЕЗУЛЬТАТ")
-    println("Восстановленный текст: \"$resultText\"")
+    println("Текст: $result")
 }
