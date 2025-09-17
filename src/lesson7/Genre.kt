03e8459d106726bf32261b809a384b4ffe8a2845
AlexDukshanin <alexdukshanin@yandex.ru>
2025-09-17 18:07:27 +0700

    7.1 finished

---
+++ src/lesson7/Genre.kt
@@ -0,0 +1,5 @@
+package lesson7
+
+enum class Genre{ // Enum class для жанров
+    FANTASY, DETECTIVE, SCIENCE, HISTORY
+}
---
+++ src/lesson7/library.kt
@@ -0,0 +1,2 @@
+// Абстрактный класс с полем id
+abstract class LibraryItem(val id: Int)
