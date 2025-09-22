803a4c8b740966c1d2a0a46d0e2734df3ec3e8d4
AlexDukshanin <alexdukshanin@yandex.ru>
2025-09-23 01:08:11 +0700

    8 lesson fix code 3

--- src/lesson8/Main.kt
+++ src/lesson8/Main.kt
@@ -2,12 +2,16 @@ package lesson8
 
 fun main() {
     val products = listOf<Product>(
-        Electronics("Смартфон", 50000.0, 10.0),
-        Clothing("Куртка", 10000.0, 15.0),
+        Electronics("Смартфон", 50000.0),
+        Clothing("Куртка", 10000.0),
         Food("Хлеб", 50.0)
     )
-    println("=== КАТАЛОГ ПРОДУКТОВ ===")
+    println("=== КАТАЛОГ ТОВАРОВ ===")
+    println()
+
     for (product in products) {
-        println("${product} -> Итоговая цена: ${"%.2f".format(product.getFinalPrice())} руб.")
+        val finalPrice = product.getFinalPrice()
+        println("${product}")
+        println("   Итоговая цена: ${"%.2f".format(finalPrice)} руб.")
     }
 }
